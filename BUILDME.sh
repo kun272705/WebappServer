bed30dfd6af0f3ca4339def0fed5e8c3d30148a1
Kun Fang <kun272705@outlook.com>
2026-02-17 12:06:59 +0800

    Renamed source/ to src/ and target/ to tgt/

--- BUILDME.sh
+++ BUILDME.sh
@@ -6,43 +6,43 @@ source .builder.sh
 
 npm install
 
-mkdir -p target/
+mkdir -p tgt/
 
-if [ -d source/pub.favicon/ ]; then
+if [ -d src/pub.favicon/ ]; then
 
-  for entry in source/pub.favicon/*; do
+  for entry in src/pub.favicon/*; do
 
-    copy_entry "$entry" "target/pub/${entry#source/pub.favicon/}" 
+    copy_entry "$entry" "tgt/pub/${entry#src/pub.favicon/}" 
   done
 fi
 
-for dir in source/pub.lib.*/; do
+for dir in src/pub.lib.*/; do
 
   for entry in "${dir}"*; do
 
-    copy_entry "$entry" "target/pub/lib/${entry#"${dir}"}"
+    copy_entry "$entry" "tgt/pub/lib/${entry#"${dir}"}"
   done
 done
 
-for dir in source/pub.*/; do
+for dir in src/pub.*/; do
  
   dir="${dir%/}"
   
   name="${dir##*.}"
 
-  dir2="${dir#source/}"
+  dir2="${dir#src/}"
   dir2="${dir2%.*}"
   dir2="${dir2//.//}"
   
   if [ -f "$dir/$name.html" ]; then
 
-    build_html "$dir/$name.html" "target/$dir2/$name.html"
+    build_html "$dir/$name.html" "tgt/$dir2/$name.html"
 
-    build_css "$dir/$name.css" "target/$dir2/$name.css"
+    build_css "$dir/$name.css" "tgt/$dir2/$name.css"
 
-    build_js "$dir/$name.js" "target/$dir2/$name.js"
+    build_js "$dir/$name.js" "tgt/$dir2/$name.js"
 
-    build_java "$dir/$name.java" "target/$dir2/$name.jar"
+    build_java "$dir/$name.java" "tgt/$dir2/$name.jar"
   fi
 done
 
